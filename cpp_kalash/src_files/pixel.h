4ff0db7f2f0185ce03b330daca09cf001fa1eb74
Kalash Shah <86906212+Kalash1106@users.noreply.github.com>
2023-03-30 10:10:44 +0530

    Add files via upload

--- cpp_kalash/src_files/pixel.h
+++ cpp_kalash/src_files/pixel.h
@@ -1,51 +1,154 @@
 #pragma once
+#include <iostream>
 #include "vec3.h"
+#include "hit_list.h"
+#include "sphere.h"
+#define sky_color color(0, 0, 200)
+#define black color(0,0,0)
+#define white color(256, 256, 256)
 
-class pixel
+
+class color
 {
 public:
-    int red, blue, green; //rgb values
-    point coordinate;
-    
-
-    pixel(point co_ord, int red_in, int blue_in, int green_in){
-        coordinate = co_ord;
+    float red, blue, green; //rgb values
+    color(){}
+    color(float red_in, float green_in, float blue_in){
         red = red_in;
         blue = blue_in;
         green = green_in;
     }
 };
 
+class pixel: public color
+{
+public:
+    color pixel_color;
+    point coordinate;
 
-pixel add_pixel_color(pixel a, pixel b, pixel c){
-    c.red = a.red + b.red;
-    c.green = a.green + b.green;
-    c.blue = a.blue + b.blue;
+    //Both the types are supported
 
-    return c;
+    pixel(point co_ord, float red_in, float green_in, float blue_in){
+        coordinate = co_ord;
+        pixel_color.red = red_in;
+        pixel_color.blue = blue_in;
+        pixel_color.green = green_in;
+    }
+
+    pixel(point co_ord, color c_in){
+        coordinate = co_ord;
+        pixel_color = c_in;
+    }
+
+    pixel(point co_ord){
+        coordinate = co_ord;
+    }
+
+    pixel(){}
+};
+
+class ray: public color
+{
+public:
+    color vec_color;
+    vec3 ray_vector;
+
+    ray(vec3 ray_in, float red_in, float green_in, float blue_in){
+        ray_vector = ray_in;
+        vec_color.red = red_in;
+        vec_color.blue = blue_in;
+        vec_color.green = green_in;
+    }
+
+    ray(vec3 ray_in, color c_in){
+        ray_vector = ray_in;
+        vec_color = c_in;
+    }
+
+    ray(vec3 ray_in){
+        ray_vector = ray_in;
+    }
+    ray(){}
+};
+
+inline double clamp(double x, double min, double max) {
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
 }
 
-pixel subtract_pixel_color(pixel a, pixel b, pixel c){
-    c.red = a.red - b.red;
-    c.green = a.green - b.green;
-    c.blue = a.blue - b.blue;
+void write_color(color a, int samples_per_pixel){
+        // Divide the color by the number of samples.
+    auto scale = 1.0 / samples_per_pixel;
+    float r = a.red * scale;
+    float g = a.green * scale;
+    float b = a.blue * scale;
 
-    return c;
+    // Write the translated [0,255] value of each color component.
+     std::cout << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
+        << static_cast<int>(256 * clamp(g, 0.0, 0.999)) << ' '
+        << static_cast<int>(256 * clamp(b, 0.0, 0.999)) << '\n';
+}
+color scale_color(color a, float scale){
+    a.red *= scale;
+    a.green *= scale;
+    a.blue *= scale;
+
+    return a;
 }
 
-pixel multiply_pixel_color(pixel a, pixel b, pixel c){
-    c.red = a.red * b.red;
-    c.green = a.green * b.green;
-    c.blue = a.blue * b.blue;
+color ray_color(const ray& r, collection_of_spheres world, hit_info rec, int n_collisions, double t_min = -INFINITY, double t_max = INFINITY) {
+    
+    std::list<sphere>::iterator it;
+    it = world.sphere_collection.begin();
 
-    return c;
+    if (hit(r.ray_vector, t_min, t_max, *it) != NAN) {
+        //root calculation
+        rec.t = hit(r.ray_vector, t_min, t_max, *it);
+
+        //Intersection point calculation
+        rec.intersection_point = add_points(r.ray_vector.origin, scale_point(r.ray_vector.direction_vector, rec.t));
+
+        //Normal vector calculation
+        rec.normal_at_intersection = normal_at_point(rec.intersection_point, *it);
+
+        //Generating the random ray
+        point target = add_points(add_points(rec.intersection_point, rec.normal_at_intersection.direction_vector),
+                                    random(r.ray_vector, rec.intersection_point));
+
+        color s_col = it-> s_col;
+        world.sphere_collection.erase(it);
+        return scale_color(
+            ray_color(ray(vec3(rec.intersection_point, subtract_points(target, rec.intersection_point)), s_col), world, rec, --n_collisions)
+            , 0.5);
+    }
+
+    else{
+        world.sphere_collection.erase(it);
+    }
+
+    point unit_direction = unit_point_vec(r.ray_vector.direction_vector);
+    auto t = 0.5*(unit_direction.y_coordinate + 1.0);
+    return operate_color(scale_color(color(1.0, 1.0, 1.0), 1.0 - t), scale_color(color(0.5, 0.7, 1.0), t));
 }
 
-pixel divide_pixel_color(pixel a, pixel b, pixel c){
-    c.red = a.red / b.red;
-    c.green = a.green / b.green;
-    c.blue = a.blue / b.blue;
 
-    return c;
+//Here operation = 1 implies addition and 1 subtraction
+color operate_color(color a, color b, int operation = 1){
+    if(operation == 1){
+        a.red = a.red + b.red;
+        a.green = a.green + b.green;
+        a.blue = a.blue + b.blue;
+
+        return a;
+    }
+    else{
+        a.red = a.red - b.red;
+        a.green = a.green - b.green;
+        a.blue = a.blue - b.blue;
+
+        return a;
+
+    }
 }
 
--- cpp_kalash/src_files/vec3.h
+++ cpp_kalash/src_files/vec3.h
@@ -1,4 +1,8 @@
 #pragma once
+#include <math.h>
+#define eye point(0,0,0)
+#define temp_vector vec3(eye, eye)
+
 class point
 {
 public:    
@@ -6,21 +10,33 @@ public:
     float y_coordinate;
     float z_coordinate;
 
-    point(float x = 0, float y = 0, float z = 0){
+    point(){}
+
+    point(float x, float y, float z){
         x_coordinate = x;
         y_coordinate = y;
         z_coordinate = z;
     }
 };
 
-point add_points(point a, point b, point c){
+point scale_point(point a, float scale){
+    point c;
+    c.x_coordinate = a.x_coordinate * scale;
+    c.y_coordinate = a.y_coordinate * scale;
+    c.z_coordinate = a.z_coordinate * scale;
+    return c;
+}
+
+point add_points(point a, point b){
+    point c(0,0,0);
     c.x_coordinate = a.x_coordinate + b.x_coordinate;
     c.y_coordinate = a.y_coordinate + b.y_coordinate;
     c.z_coordinate = a.z_coordinate + b.z_coordinate;
     return c;
 }
 
-point subtract_points(point a, point b, point c){
+point subtract_points(point a, point b){
+    point c(0,0,0);
     c.x_coordinate = a.x_coordinate - b.x_coordinate;
     c.y_coordinate = a.y_coordinate - b.y_coordinate;
     c.z_coordinate = a.z_coordinate - b.z_coordinate;
@@ -28,14 +44,16 @@ point subtract_points(point a, point b, point c){
 }
 
 
-point multiply_points(point a, point b, point c){
+point multiply_points(point a, point b){
+    point c(0,0,0);
     c.x_coordinate = a.x_coordinate * b.x_coordinate;
     c.y_coordinate = a.y_coordinate * b.y_coordinate;
     c.z_coordinate = a.z_coordinate * b.z_coordinate;
     return c;
 }
 
-point divide_points(point a, point b, point c){
+point divide_points(point a, point b){
+    point c(0,0,0);
     c.x_coordinate = a.x_coordinate / b.x_coordinate;
     c.y_coordinate = a.y_coordinate / b.y_coordinate;
     c.z_coordinate = a.z_coordinate / b.z_coordinate;
@@ -48,12 +66,36 @@ class vec3 : public point
 public:
     point origin;
     float t;
-    point direction_vector;    
+    point direction_vector; 
 
+    vec3(){}   
+
+    //Vector having origin o_in, parameter t_in and direction vector b_in which would be made unit automatically
     vec3(point o_in, float t_in, point b_in){
         origin = o_in;
         t = t_in;
         direction_vector = b_in;
+
+        make_dirn_vec_unit();
+    }
+
+    //vector passing through 2 points a and b
+    vec3(point a, point b){
+        origin = a;
+        direction_vector = subtract_points(b,a);
+        make_dirn_vec_unit();
+        t = 1;
+    }
+
+    void make_dirn_vec_unit(){
+        float l2_a = sqrt(direction_vector.x_coordinate * direction_vector.x_coordinate +
+                          direction_vector.y_coordinate * direction_vector.y_coordinate +
+                          direction_vector.z_coordinate * direction_vector.z_coordinate);
+    direction_vector.x_coordinate /= l2_a;
+    direction_vector.y_coordinate /= l2_a;
+    direction_vector.z_coordinate /= l2_a;
+    return;
+
     }
 };
 
@@ -65,36 +107,77 @@ point end_point_of_vector(vec3 a){
     return tip;
 }
 
-vec3 dot_product(vec3 a, vec3 b, vec3 c){
-    c.direction_vector.x_coordinate = a.direction_vector.x_coordinate * b.direction_vector.x_coordinate;
-    c.direction_vector.y_coordinate = a.direction_vector.y_coordinate * b.direction_vector.y_coordinate;
-    c.direction_vector.z_coordinate = a.direction_vector.z_coordinate * b.direction_vector.z_coordinate;
+
+float l2_norm(vec3 a){
+    point tip = end_point_of_vector(a);
+    float l2 = sqrt(tip.x_coordinate * tip.x_coordinate + 
+                    tip.y_coordinate * tip.y_coordinate + 
+                    tip.z_coordinate * tip.z_coordinate);
+    return l2;
+}
+
+float l2_point_norm(point a_in){
+    float l2 = sqrt(a_in.x_coordinate * a_in.x_coordinate + 
+                    a_in.y_coordinate * a_in.y_coordinate + 
+                    a_in.z_coordinate * a_in.z_coordinate);
+    return l2;
+
+}
+
+point unit_point_vec(point a){
+    point tip_a;
+    float l2_a = sqrt(a.x_coordinate * a.x_coordinate +
+                 a.y_coordinate * a.y_coordinate +
+                 a.z_coordinate * a.z_coordinate);
+    tip_a.x_coordinate /= l2_a;
+    tip_a.y_coordinate /= l2_a;
+    tip_a.z_coordinate /= l2_a;
+    return tip_a;
+}
+
+point unit_vector(vec3 a){
+    point tip_a = end_point_of_vector(a);
+    float l2_a = l2_norm(a);
+    tip_a.x_coordinate /= l2_a;
+    tip_a.y_coordinate /= l2_a;
+    tip_a.z_coordinate /= l2_a;
+    return tip_a;
+}
+
+float dot_product(point a, point b){
+    float dot = a.x_coordinate * b.x_coordinate +
+                a.y_coordinate * b.y_coordinate +
+                a.z_coordinate * b.z_coordinate;
+    return dot;
+}
+
+//@brief Vector product of two vectors. Make sure to have origin as (0,0,0) and t = 1 
+point cross_product(point a, point b){
+    point c;
+    c.x_coordinate = a.y_coordinate * b.z_coordinate - 
+                    a.z_coordinate * b.y_coordinate;
+
+    c.y_coordinate = - (a.x_coordinate * b.z_coordinate) + 
+                        a.z_coordinate * b.x_coordinate;
+
+    c.z_coordinate = a.x_coordinate * b.y_coordinate
+                                    - a.y_coordinate * b.x_coordinate;
+
     return c;
 }
 
-vec3 cross_product(vec3 a, vec3 b, vec3 c){
-    c.direction_vector.x_coordinate = a.direction_vector.y_coordinate * b.direction_vector.z_coordinate
-                                    - a.direction_vector.z_coordinate * b.direction_vector.y_coordinate;
 
-    c.direction_vector.y_coordinate = - (a.direction_vector.x_coordinate * b.direction_vector.z_coordinate)
-                                    + a.direction_vector.z_coordinate * b.direction_vector.x_coordinate;
-
-    c.direction_vector.z_coordinate = a.direction_vector.x_coordinate * b.direction_vector.y_coordinate
-                                    - a.direction_vector.y_coordinate * b.direction_vector.x_coordinate;
-
+vec3 vector_addition(vec3 a, vec3 b){
+    vec3 c(point(0,0,0), 0, point(0,0,0));
+    c.origin = add_points(a.origin, b.origin);
+    c.direction_vector = add_points(a.direction_vector, b.direction_vector);
     return c;
 }
 
-
-vec3 vector_addition(vec3 a, vec3 b, vec3 c){
-    c.origin = add_points(a.origin, b.origin, c.origin);
-    c.direction_vector = add_points(a.direction_vector, b.direction_vector, c.direction_vector);
-    return c;
-}
-
-vec3 vector_subtraction(vec3 a, vec3 b, vec3 c){
-    c.origin = subtract_points(a.origin, b.origin, c.origin);
-    c.direction_vector = subtract_points(a.direction_vector, b.direction_vector, c.direction_vector);
+vec3 vector_subtraction(vec3 a, vec3 b){
+    vec3 c(point(0,0,0), 0, point(0,0,0));
+    c.origin = subtract_points(a.origin, b.origin);
+    c.direction_vector = subtract_points(a.direction_vector, b.direction_vector);
     return c;
 }
 
